769f054d9303664b64d6dcedda7714cb142eb23a
markov98 <74935630+markov98@users.noreply.github.com>
2024-04-14 11:20:44 +0300

    added card form component, added html to component, deleted uneeded routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardTemplateComponent } from './card-template/card-template.component';
+import { CardFormComponent } from './card-form/card-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardTemplateComponent],
+  imports: [RouterOutlet, CardTemplateComponent, CardFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
