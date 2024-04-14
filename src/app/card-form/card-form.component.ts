9c5659286f20d089a0f3af6d68e9d1bad6613b68
markov98 <74935630+markov98@users.noreply.github.com>
2024-04-14 11:35:52 +0300

    turned form into a reactive form

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CardValuesService } from '../card-values.service';
 
 @Component({
   selector: 'app-card-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './card-form.component.html',
   styleUrl: './card-form.component.css'
 })
 export class CardFormComponent {
+  constructor(private cardService: CardValuesService) { }
 
+  onSubmit(form: NgForm): void { }
 }
