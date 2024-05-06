0ade296b80a7d99549aba5353cd03bdfe1b2fe8a
markov98 <74935630+markov98@users.noreply.github.com>
2024-05-06 07:45:05 +0300

    ádded tests to card-template component

--- src/app/card-template/card-template.component.spec.ts
+++ src/app/card-template/card-template.component.spec.ts
@@ -20,4 +20,14 @@ describe('CardTemplateComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain card values', () => {
+    expect(component.cardValues).toBeTruthy();
+  });
+
+  it('should contain h2 with cardValues.name', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const cardTitle = bannerElement.querySelector('h2')!;
+    expect(cardTitle.textContent).toEqual(component.cardValues.name);
+  });
 });
