fd914c1a677b2af56398f4a3749e11b4ab34bb7d
markov98 <74935630+markov98@users.noreply.github.com>
2024-05-06 16:17:58 +0300

    updated card template test

--- src/app/card-template/card-template.component.spec.ts
+++ src/app/card-template/card-template.component.spec.ts
@@ -30,4 +30,16 @@ describe('CardTemplateComponent', () => {
     const cardTitle = bannerElement.querySelector('h2')!;
     expect(cardTitle.textContent).toEqual(component.cardValues.name);
   });
+
+  it('should contain p with cardValues.class', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const cardClass = bannerElement.querySelector('.card-class')!;
+    expect(cardClass.textContent).toEqual(component.cardValues.class);
+  });
+
+  it('should contain p with cardValues.descrtipiton', () => {
+    const bannerElement: HTMLElement = fixture.nativeElement;
+    const cardDescription = bannerElement.querySelector('.card-desc')!;
+    expect(cardDescription.textContent).toEqual(component.cardValues.description);
+  });
 });
