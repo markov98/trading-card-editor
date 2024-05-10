37c31f8b8dabce2f0e61ac7160b661c0e1c3d318
markov98 <74935630+markov98@users.noreply.github.com>
2024-05-10 11:17:31 +0300

    added capturing image functionality

--- src/app/card-template/card-template.component.ts
+++ src/app/card-template/card-template.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CardValuesService } from '../card-values.service';
 import { CardInfo } from '../types/CardInfo';
 import { CommonModule } from '@angular/common';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-card-template',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './card-template.component.html',
-  styleUrl: './card-template.component.css'
+  styleUrls: ['./card-template.component.css']
 })
 export class CardTemplateComponent {
+  @ViewChild('card') cardTemplate!: ElementRef;
 
   get cardValues(): CardInfo {
     return this.valuesService.cardValues;
   }
 
-  constructor (private valuesService: CardValuesService) { }
+  constructor(private valuesService: CardValuesService) { }
+
+  captureImage() {
+    html2canvas(this.cardTemplate.nativeElement).then(canvas => {
+      const image = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = 'card_image.png';
+      link.click();
+    });
+  }
 }
